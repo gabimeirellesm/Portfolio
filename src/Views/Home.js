8a92ae56d439b5c0b62edc7c69b0fd96560792cf
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-04-01 23:26:39 +0100

    video home page

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import gifHomePage from "../Images/gifHomePage.gif";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img src={gifHomePage} alt="video Gabriela Meirelles" />
+      <style jsx="true">{`
+        img {
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      `}</style>
+    </div>
+  );
 }
 
 export default Home;
