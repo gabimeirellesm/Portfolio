55a366906a00cd3f873a37ec4988a1b1dd2b36bf
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-04-01 20:34:47 +0100

    included Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import React from "react";
+import Navbar from "../src/Components/Navbar.js";
+import Home from "./Views/Home";
+import About from "./Views/About";
+import Projects from "./Views/Projects";
+import Contact from "./Views/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <p>My portfolio</p>
+    <div>
+      <Navbar />
+      <section id="section1">
+        <Home />
+      </section>
+      <section id="section2">
+        <About />
+      </section>
+      <section id="section3">
+        <Projects />
+      </section>
+      <section id="section4">
+        <Contact />
+      </section>
     </div>
   );
 }
